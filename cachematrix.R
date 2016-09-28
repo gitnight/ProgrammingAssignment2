a742f0075ac299ef5f4283c570238a0976a391c0
gitnight <gitnight@users.noreply.github.com>
2016-09-27 23:58:33 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,30 @@
-#Functions to create a matrix and cace its invrse
+#Functions to create a matrix and cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) { #create function
-        inverse1 <- NULL                             #assign null value to inverse
+        inv <- NULL                             #assign null value to inverse
         set <- function(y) {
                 x <<- y                 #globally assign inputted value to x, which would be taken into inverse calculation
-                inverse1 <<- NULL            #globally set inv to NULL
+                inv <<- NULL            #globally set inv to NULL
         }
         get <- function() x #return value of matrix
-        setinv = function(inverse2) inverse2 <<- inverse1 #set value
-        getinv = function() inverse1 #output inverse value
-        list(set=set, get=get, setinv=setinv, getinv=getinv)
+        setinverse <- function(inverse) inv <<- inverse #set value
+        getinverse <- function() inv #output inverse value
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-cacheSolve <- function(x) {
+cacheSolve <- function(x, ...) {
         
-        inverse1 = x$getinv() #get any existing value of inverse from cache
+        inv <- x$getinverse() #get any existing value of inverse from cache
         
-        if (!is.null(inverse1)){ #check if existing value is there, if yes, return that value
+        if (!is.null(inv)){ #check if existing value is there, if yes, return that value
                 message("getting cached data")
-                return(inverse1)
+                return(inv)
         }
         
-       matrix.data = x$get() #if no existing value of inverse in cache get the matrix data
-        inverse1 = solve(matrix.data) #calculate inverse
+       data <- x$get() #if no existing value of inverse in cache get the matrix data
+        inv <- solve(data, ...) #calculate inverse
         
-        x$setinv(inverse1) #set value in cache
+        x$setinverse(inv) #set value in cache
         
-        return(inverse1) #return the inverse
+        inv #return the inverse
 }
